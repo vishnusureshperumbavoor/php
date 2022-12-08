85487425072a9ceb99bc1556d3342b84f88d2b66
vsp <vishnusureshperumbavoor@gmail.com>
2022-12-08 15:15:32 +0530

    heloo

--- mysql/posthandling.php
+++ mysql/posthandling.php
@@ -1,21 +1,41 @@
 <?php 
 # database connection
-$_SERVER="locahost";
-$_USERNAME = "root";
-$_PASSWORD = "mysql";
-$_CONN = new mysqli($_SERVER,$_USERNAME,$_PASSWORD);
-if($_CONN->connect_eroor){
-    die("Connection failed : " . $_CONN->connect_error . "<br>");
-}
+$server ="locahost";
+$username = "root";
+$password = "mysql";
+$dbname = "vspdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
 else{
-    echo "Connection Successfull<br>";
+    echo "Connection successful<br>";
 }
 
-# database creation
-$sql = "CREATE DATABASE VSPDB";
+$username = $_POST["name"];
+$userpassword = $_POST["password"];
+
+$sql = "INSERT INTO userdetails VALUES (NULL,'$username','$userpassword')";
+    
 if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
-} else {
-  echo "Error creating database: " . $conn->error;
-}
-$_CONN->close();
+    echo "New record created successfully<br>";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  } 
+
+  $sql = "SELECT * FROM userdetails";
+  $result = $conn->query($sql);
+  
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["userid"]. "\tName: " . $row["username"]. "\tPassword : " . $row["userpassword"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+
+$conn->close();
